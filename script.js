9c76b0786fcf56cd2ec3fe6dcdc338399ff4348d
smo-haenle <your_email@youremail.com>
2021-06-09 18:37:52 +0200

    bugfix1

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ window.addEventListener("load", function () {
     console.log("lockedandloaded");
     //let form: HTMLFormElement = document.querySelector("#myform");
     let index = 1;
+    let url = "http://localhost:5002";
+    let test = document.querySelector("#fname");
+    let info = {
+        name: "",
+        spiritanimals: "",
+        favband: "",
+        "songs": {
+            "vid1": "nice",
+            "vid2": "nice",
+            "vid3": "nice",
+            "vid4": "nice",
+            "vid5": "nice"
+        }
+    };
+    document.querySelector("#letsgo").addEventListener("click", letsgo);
+    document.querySelector("#click").addEventListener("click", submitandstart);
+    document.querySelector("#like").addEventListener("click", counter);
+    document.querySelector("#nolike").addEventListener("click", counter);
     //SCREENS
     function letsgo() {
         console.log("letsgo");
@@ -11,17 +29,29 @@ window.addEventListener("load", function () {
         document.getElementById("swipesite").style.display = "none";
         document.getElementById("matches").style.display = "none";
     }
-    document.querySelector("#letsgo").addEventListener("click", letsgo);
     function submitandstart() {
-        console.log("submitandstart");
-        document.getElementById("start").style.display = "none";
-        document.getElementById("formsite").style.display = "none";
-        document.getElementById("swipesite").style.display = "block";
-        document.getElementById("matches").style.display = "none";
-        changevid();
+        if (test.value.length < 3) {
+            alert("type your name fr");
+        }
+        else {
+            document.getElementById("start").style.display = "none";
+            document.getElementById("formsite").style.display = "none";
+            document.getElementById("swipesite").style.display = "block";
+            document.getElementById("matches").style.display = "none";
+            changevid();
+            submitData();
+            console.log("submitandstart");
+        }
     }
-    document.querySelector("#click").addEventListener("click", submitandstart);
-    document.querySelector("#submitbutton").addEventListener("click", submitandstart);
+    function submitData() {
+        let nameInput = document.querySelector("#fname").value;
+        let animalInput = document.querySelector("#fanimal").value;
+        let bandInput = document.querySelector("#fband").value;
+        info.name = nameInput;
+        info.spiritanimals = animalInput;
+        info.favband = bandInput;
+        console.log(info.name, info.spiritanimals, info.favband);
+    }
     function matches() {
         document.getElementById("start").style.display = "none";
         document.getElementById("formsite").style.display = "none";
@@ -30,13 +60,21 @@ window.addEventListener("load", function () {
     }
     //VIDEO
     function changevid() {
-        document.getElementById("vidsrc").src = "vid" + index + ".mp4";
+        document.getElementById("vidsrc").src = "assets/" + "vid" + index + ".mp4";
         //document.querySelector("video")!.play
         console.log("vid" + index + ".mp4");
     }
     //COUNTER
-    function counter() {
-        console.log("counter");
+    function counter(_e) {
+        let vid = "vid" + index.toString;
+        if (_e.target.id == "like") {
+            console.log(vid);
+            info.songs["vid" + index] = "nice";
+        }
+        else if (_e.target.id == "nolike") {
+            console.log(vid);
+            info.songs["vid" + index] = "whack";
+        }
         index += 1;
         console.log(index);
         if (index <= 5) {
@@ -45,11 +83,34 @@ window.addEventListener("load", function () {
         }
         else {
             console.log("stooooop");
+            sendToServer();
+            let match = getDataAndCompare();
             matches();
         }
     }
-    document.querySelector("#like").addEventListener("click", counter);
-    document.querySelector("#nolike").addEventListener("click", counter);
+    async function sendToServer() {
+        let stringy = JSON.stringify(info);
+        let infoOb = {
+            info: stringy
+        };
+        let query = new URLSearchParams(infoOb);
+        let response = await fetch(url + "/save?" + query.toString());
+        let responseJSON = await response.json();
+        console.log(await responseJSON);
+        //json string wird zu einem query string umgewandelt
+        //alert("Saved");
+    }
+    async function getDataAndCompare() {
+        let searchOb = {
+            search: "search"
+        };
+        let query = new URLSearchParams(searchOb);
+        let response = await fetch(url + "/save?" + query.toString());
+        let responseStr = await response.json();
+        let responseJSON = JSON.parse(await responseStr);
+        console.log(responseJSON);
+        return responseJSON;
+    }
     /* form!.onsubmit = () => {
          let formData: FormData = new FormData(form);
  
